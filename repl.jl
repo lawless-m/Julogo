86c22084ec7bd4131be48412f6b14f8a534d0597
lawless-m <maht0x0r@xsmail.com>
2016-10-16 16:04:21 -0400

    hard work

--- repl.jl
+++ repl.jl
@@ -7,26 +7,60 @@ end
 
 stack = Any[]
 
-cmds = Dict{AbstractString, Function}("fd"=>Turtles.forward, "rt"=>Turtles.right, "+"=>+)
+const CMDS = ["~" "FD" "FORWARD" "BK" "BACK" "LEFT" "LT" "RIGHT" "RT" "PU" "PENUP" "PD" "PENDOWN" "EXIT"]
+
+
+function expr(ln)
+	tok, lnz = split(ln, [' ', '\n']; limit=2)
+	p = parse(tok)
+	if tok == "~"
+		if ln != "\n"
+			@printf STDERR "unexpected ~\n"
+			return ""
+		end
+		return lnz
+	end
+	if typeof(p) == Symbol
+		@printf STDERR "symbol %s" p
+		return lnz
+	end
+	if isnumber(p)
+		@printf STDERR "numeric %f" p
+		return lnz
+	end
+	
+	@printf STDERR "expecting expr"
+	return ""
+end
+
+function cmd(ln)
+	tok, lnz = split(ln, [' ', '\n']; limit=2)
+	@printf STDERR "t:%s \n" tok
+	if ! (string(tok) in CMDS)
+		@printf STDERR "Expecting one of: %s\n" CMDS
+		return true
+	end
+	if tok == "~"
+		return false
+	end
+	if tok == "EXIT"
+		quit()
+	end
+	if ln == ""
+		@printf STDERR "expecting expression, got BLANK"
+		return true
+	end
+	lnz = expr(lnz)
+	if ln != ""
+		return cmd(lnz)
+	end
+	return true
+end
+
 
 prompt()
 for ln in eachline(STDIN)
-	sp = split(ln, [' ', '\n']; limit=2)
-	while size(sp)[1] > 1
-		tok, ln = sp
-		if haskey(cmds, tok)
-			push!(stack, cmds[tok])
-		else
-			n = tryparse(Float64, tok)
-			if isnull(n)
-				println("Expecting numeric")
-				break
-			end
-			push!(stack, get(n))
-		end
-		sp = split(ln, [' ', '\n']; limit=2)
+	if cmd(ln)
+		prompt()
 	end
-	println(stack)
-	stack = Any[]
-	prompt()
 end
