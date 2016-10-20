0fd47b2db48946fc39c859adebb14854a5629016
lawless-m <maht0x0r@xsmail.com>
2016-10-20 12:44:56 -0400

    lexing via overload

--- Lex.jl
+++ Lex.jl
@@ -1,9 +1,26 @@
 
-
 @enum CMDS fd forward bk back left lt right rt pu penup pd pendown exit
 
-type Lex
-	cmd::CMDS
+type Cmd
+	fn::Function
+	es::Vector{Expr}
+	Cmd(fn) = new(fn, Expr[])
+end
+
+type Expr
+	op::Function
+	es::Vector{Union{Expr, Float64}}
+	Expr(op::Function) = new(op, Expr[])
+	Expr(n::Float64) = new((x)->x, Expr[n])
+	Expr(op::Function, e) = new(op, Expr[e])
+end
+
+function evaluate(e::Expr)
+	e.op(map(evaluate, e.es))
+end
+
+function execute(c::CmdA1)
+	c.fn(map(evaluate, c.es)...)
 end
 
 prompt = "L> "
@@ -17,7 +34,9 @@ function input()
 				if typeof(eval(Symbol(p))) == CMDS
 					produce(Lex(eval(Symbol(p))))
 				else
-					produce(p)
+					if isnumber(p)
+						produce(Expr(parse(Float64, p)))
+					end
 				end
 				
 			end
@@ -26,8 +45,11 @@ function input()
 	end
 end
 
+stack = []
+
 
 for t in Task(input)
+	
 	if typeof(t) == Lex
 		if t.cmd == exit
 			quit()
@@ -42,4 +64,3 @@ for t in Task(input)
 	@printf "L:%s - %s\n" t typeof(t)
 end
 
-	
